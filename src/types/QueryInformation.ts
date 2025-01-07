44cf02cb70dc35fc077d744059450a80bc24473a
VonRiddarn <vonriddarn@gmail.com>
2025-01-07 13:24:15 +0100

    feat: renamed queryinformation to search result and made it a generic object

--- src/types/QueryInformation.ts
+++
@@ -1,8 +0,0 @@
-type QueryInformation = {
-	count: number;
-	pages: number;
-	next: string | null;
-	prev: string | null;
-};
-
-export default QueryInformation;
---
+++ src/types/SearchResult.ts
@@ -0,0 +1,16 @@
+import Character from "./Character";
+import Episode from "./Episode";
+import Place from "./Place";
+
+type SearchResult<T> = {
+	error?: string;
+	info: {
+		count: number;
+		pages: number;
+		next: string | null;
+		prev: string | null;
+	};
+	results: T[];
+};
+
+export default SearchResult;
