fda3a6d0d9c8ecb2ac2fe385a939a6b27b0b4525
VonRiddarn <vonriddarn@gmail.com>
2025-01-08 10:29:06 +0100

    feat: added test buttons

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 // TODO: Create categories and clone cors anywhere to local
 // https://dev.to/saiavinashiitr/comment/19df0
 // https://github.com/Rob--W/cors-anywhere
+const main = document.querySelector("main") as HTMLElement;
 
-import getCharacters from "./utilities";
+import {getCharacters, getPlaces} from "./utilities";
 
+let cp = 1;
+let lp = 1;
 
-getCharacters(4);
+document.getElementById('characters-button')?.addEventListener('click', () => {
+	main.innerHTML = "";
+	getCharacters(cp++);
+});
+
+document.getElementById('locations-button')?.addEventListener('click', () => {
+	main.innerHTML = "";
+	getPlaces(lp++);
+});
+
+document.getElementById('episodes-button')?.addEventListener('click', () => {
+	main.innerHTML = "";
+});
