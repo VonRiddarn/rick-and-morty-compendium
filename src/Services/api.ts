b24f3b3d83ea6e17a33dc8f846a36b4483a437c5
VonRiddarn <vonriddarn@gmail.com>
2025-01-10 14:09:33 +0100

    feat, refactor: restructured api to let caller use the generic method with no wrapper

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,22 +1,39 @@
 import Character from "../types/Character";
 import Endpoint from "../types/Endpoint";
-import Episode from "../types/Episode";
-import Place from "../types/Place";
 import SearchResult from "../types/SearchResult";
 
 const API_ROOT = "https://rickandmortyapi.com/api/";
 
-const api = Object.freeze({
+const buildEndPoint = (endpoint: Endpoint): string | null => `${API_ROOT}${endpoint}/`;
+
+const api = {
+	// Generic searches (Possible because the API is structured <3)
+	getObjectFromId: async <T>(id: Number, endpoint:Endpoint): Promise<T | null> => {
+		const response = await fetch(`${buildEndPoint(endpoint)}/${id}`);
+		return await response.json() as T | null;
+	},
+	getObjectsFromIds: async <T>(ids: Number[], endpoint:Endpoint): Promise<T[]> => {
+		const response = await fetch(`${buildEndPoint(endpoint)}${ids.join(",")}`);
+		const data = await response.json() as T[] | null;
+
+		return data === null ? [] : data;
+	},
+	getObjectFromUrl: async <T>(url: string): Promise<T | null> => {
+		const response = await fetch(url);
+		return await response.json() as T | null;
+	},
+	getObjectsFromUrls: async <T>(urls: string[]): Promise<T[]> => {
+		const response = await Promise.all(urls.map(url => api.getObjectFromUrl<T>(url)));
+		return response.filter(e => e !== null) as T[];
+	},
+	getObjectsFromPage: async <T>(page: Number, endpoint:Endpoint): Promise<T[]> => {
+		const response = await fetch(`${buildEndPoint(endpoint)}?page=${page}`);
+		const data = await response.json() as SearchResult<T> | null;
+
+		return data === null ? [] : data.results;
+	},
+	// Type specific searches
 	getCharacters: {
-		fromIds: async (ids: number[]): Promise<Character[]> => {
-			return await getObjectsFromIds<Character>(ids, "character");
-		},
-		fromUrls: async (urls: string[]): Promise<Character[]> => {
-			return await getObjectsFromUrls<Character>(urls);
-		},
-		fromPage: async (page: number): Promise<Character[]> => {
-			return await getObjectsFromPage<Character>(page, "character");
-		},
 		fromName: async (name: string): Promise<Character[]> => {
 			const response = await fetch(`${buildEndPoint("character")}?name=${name}`);
 			const data = await response.json() as SearchResult<Character> | null;
@@ -24,56 +41,6 @@ const api = Object.freeze({
 			return data === null ? [] : data.results;
 		}
 	},
-	getCharacter: {
-		fromId: async (id: number): Promise<Character | null> => {
-			return await getObjectFromId<Character>(id, "character");
-		},
-		fromUrl: async (url: string): Promise<Character | null> => {
-			return await getObjectFromUrl<Character>(url);
-		}
-	},
-	getPlaces: {
-		fromIds: async (ids: Number[]): Promise<Place[]> => {
-			return getObjectsFromIds<Place>(ids, "location");
-		},
-		fromPage: async (page: Number): Promise<Place[]> => {
-			return await getObjectsFromPage<Place>(page, "location");
-		},
-	}
-});
-
-const buildEndPoint = (endpoint: Endpoint): string | null => `${API_ROOT}${endpoint}/`;
-
-// Generics (Possible because the API is structured <3)
-/* ----- ID -----*/
-const getObjectFromId = async <T>(id: Number, endpoint:Endpoint): Promise<T | null> => {
-	const response = await fetch(`${buildEndPoint(endpoint)}/${id}`);
-	return await response.json() as T | null;
-}
-
-const getObjectsFromIds = async <T>(ids: Number[], endpoint:Endpoint): Promise<T[]> => {
-	const response = await fetch(`${buildEndPoint(endpoint)}${ids.join(",")}`);
-	const data = await response.json() as T[] | null;
-
-	return data === null ? [] : data;
-}
-
-/* ----- URL -----*/
-const getObjectFromUrl = async <T>(url: string): Promise<T | null> => {
-	const response = await fetch(url);
-	return await response.json() as T | null;
-}
-const getObjectsFromUrls = async <T>(urls: string[]): Promise<T[]> => {
-	const response = await Promise.all(urls.map(url => getObjectFromUrl<T>(url)));
-	return response.filter(e => e !== null) as T[];
-}
-
-/* ----- PAGE ----- */
-const getObjectsFromPage = async <T>(page: Number, endpoint:Endpoint): Promise<T[]> => {
-	const response = await fetch(`${buildEndPoint(endpoint)}?page=${page}`);
-	const data = await response.json() as SearchResult<T> | null;
-
-	return data === null ? [] : data.results;
-}
+} as const;
 
 export default api;
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,24 @@
 const main = document.querySelector("main") as HTMLElement;
 
 import api from "./Services/api";
+import Character from "./types/Character";
+import Place from "./types/Place";
 
 let cp = 1;
 let lp = 1;
+let ep = 1;
 
 document.getElementById('characters-button')?.addEventListener('click', async () => {
 	main.innerHTML = "";
-	console.log(await api.getCharacters.fromPage(cp++));
+	console.log(await api.getObjectsFromPage<Character>(cp++, "character"));
 });
 
 document.getElementById('locations-button')?.addEventListener('click', async () => {
 	main.innerHTML = "";
-	console.log(await api.getPlaces.fromPage(lp++));
+	console.log(await api.getObjectsFromPage<Place>(lp++, "location"));
 });
 
-document.getElementById('episodes-button')?.addEventListener('click', () => {
+document.getElementById('episodes-button')?.addEventListener('click', async () => {
 	main.innerHTML = "";
+	console.log(await api.getObjectsFromPage(ep++, "episode"));
 });
