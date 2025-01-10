b95c0c2276d2c34017a9ba883b27df6613e7e73c
VonRiddarn <vonriddarn@gmail.com>
2025-01-10 10:11:45 +0100

    docs: removed redundant comments

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-// TODO: Create categories and clone cors anywhere to local
-// https://dev.to/saiavinashiitr/comment/19df0
-// https://github.com/Rob--W/cors-anywhere
 const main = document.querySelector("main") as HTMLElement;
 
 import api from "./Services/api";
