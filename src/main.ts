e7c80c45c69eea8dd74deda128884d0e8840ac11
VonRiddarn <vonriddarn@gmail.com>
2025-01-20 11:39:14 +0100

    docs: added todo for page states

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,11 @@ import "./style.scss";
 const body = document.querySelector("body") as HTMLElement;
 
 body.appendChild(document.createElement("p")).innerHTML = "Hello world!";
+
+
+// TODO: Add pageStateMachine
+/* 
+Page state machine or "page manager" manages the state of pages.
+When chanign state from, say, "main" to "favorites", the header is updated and the main node is switched to
+the appropriate cahced one.
+*/
