c523a89b19232a8f4629db6a44f8296da46be398
VonRiddarn <vonriddarn@gmail.com>
2025-02-03 16:08:58 +0100

    feat: refactored modal to easier accept different content.

--- src/components/modal/modal.ts
+++ src/components/modal/modal.ts
@@ -1,32 +1,56 @@
-import { Entity } from "../../types/api.types";
+import { Character, Entity } from "../../types/api.types";
 import { parseUrl } from "../../utils/api.utils";
 import "./modal.scss";
 
-let currentModal: HTMLElement | null = null; 
+type Modal = {
+	modalNode: HTMLElement | null;
+	contentNode: HTMLElement | null;
+}
+
+let currentRoot: HTMLElement | null = null; 
+const currentModal:Modal = {
+	modalNode: null,
+	contentNode: null
+}
 
 const updateModal = (content?: HTMLElement) => {
-	const root = document.createElement("div");
-	root.classList.add("modal");
 
-	const modal = root.appendChild(document.createElement("div"));
-	
-	root.addEventListener('click', (event) => {
-		if(event.target === root) 
-			killModal(root)
-	});
+	if(currentRoot === null)
+	{
+		currentRoot = document.createElement("div");
+		currentRoot.classList.add("modal");
 
-	const killModal = (modalRoot:HTMLElement) => {
-		currentModal = null;
-		modalRoot.remove();
+		currentRoot.addEventListener('click', (event) => {
+			if(event.target === currentRoot) 
+				killModal(currentRoot as HTMLElement);
+		});
+
+		currentModal.modalNode = currentRoot.appendChild(document.createElement("div"));
+		currentModal.modalNode.appendChild(document.createElement("Button")).innerHTML = "<";
+		currentModal.modalNode.appendChild(document.createElement("Button")).innerHTML = ">";
+		currentModal.modalNode.appendChild(document.createElement("Button")).innerHTML = "X";
+		
+		currentModal.contentNode = currentModal.modalNode.appendChild(document.createElement("div"));
+
+		document.querySelector("body")?.appendChild(currentRoot) as HTMLElement;
+	}
+	else
+	{
+		currentRoot.classList.remove("force-hidden");
 	}
 
 	if(content)
-		modal.appendChild(content);
+	{
+		currentModal.contentNode?.replaceWith(content);
+		currentModal.contentNode = content;
+	}
+}
 
-	if(currentModal !== null)
-		currentModal = root;
-	else
-		currentModal = document.querySelector("body")?.appendChild(root) as HTMLElement;
+const killModal = (modalRoot:HTMLElement) => {
+	modalRoot.classList.add("force-hidden");
+
+	// Clear content when closing modal
+	currentModal.contentNode!.innerHTML = "";
 }
 
 export const openFilterModal = () => {
@@ -40,7 +64,7 @@ export const openEntityModal = (entity:Entity) => {
 	switch(type)
 	{
 		case "character":
-		contentMethod = getErrorModal(entity.name);
+		contentMethod = getCharacterModal(entity as Character);
 		break;
 
 		case "location":
@@ -56,8 +80,17 @@ export const openEntityModal = (entity:Entity) => {
 }
 
 const getErrorModal = (msg:string) => {
-	const p = document.createElement("p");
-	p.innerHTML = msg;
 
-	return p;
+	const container = document.createElement("div")
+	container.appendChild(document.createElement("p")).innerHTML = msg;
+
+	return container;
+}
+
+const getCharacterModal = (character:Character) => {
+	const container = document.createElement("div");
+
+	container.appendChild(document.createElement("h2")).innerHTML = character.name;
+
+	return container;
 }
