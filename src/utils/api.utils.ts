65cedd3fa79fdc59a679b9f7daf470996a017546
VonRiddarn <vonriddarn@gmail.com>
2025-02-05 21:13:54 +0100

    feat: moved method "getEntityImage" into api.utils. REASON: We will want to use this across several components, so it'd make more sense for it to live globally / separate, rather than inside of a component.

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,5 +1,5 @@
 import api from "../services/api";
-import { Endpoint, Entity, Episode } from "../types/api.types";
+import { Character, Endpoint, Entity, Episode } from "../types/api.types";
 
 export const parseSignature = (signature: string): {season:number, episode:number} => {
 	
@@ -32,3 +32,49 @@ export const getEpisodeNameFromUrl = async (url: string) => {
 
 	return ret ? ret : "UNKNOWN";
 }
+
+export const getEntityImage = (entity:Entity):HTMLElement => {
+
+	switch(getEndpointName(entity))
+	{
+
+		case "character":
+		const cImg = document.createElement("img");
+		cImg.src = (entity as Character).image;
+		return cImg;
+
+		case "location":
+		const lImg = document.createElement("img");
+		lImg.src = "/public/images/Icon_Location.png";
+		return lImg;
+
+		case "episode":
+		const eSvg = document.createElement("div");
+		fetch("/public/images/Icon_Episode_Dynamic.svg")
+		.then(response => response.text())
+		.then(svgContent => {
+            eSvg.innerHTML = svgContent;
+
+            const { season, episode } = parseSignature((entity as Episode).episode);
+
+            setTimeout(() => {
+                const seasonText = eSvg.querySelector("#seasonText");
+                const episodeText = eSvg.querySelector("#episodeText");
+
+                if (seasonText) seasonText.textContent = `Season ${season}`;
+                if (episodeText) episodeText.textContent = `Episode ${episode}`;
+            }, 0);
+        })
+		.catch(error => {
+			console.error("Error loading svg:",error);
+			eSvg.innerHTML = "Error";
+		})
+		return eSvg;
+
+		default:
+		console.error("COULD NOT FIND ENDPOINT OF CARD!");
+		const errorImg = document.createElement("img");
+		errorImg.src = "/public/images/Icon_Error.png";
+		return errorImg;
+	}
+} 
