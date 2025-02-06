4c6b7a4adb346e7a1c42527b7f3f68d31b4f1ba9
VonRiddarn <vonriddarn@gmail.com>
2025-02-06 13:14:32 +0100

    feat: added better save logic for notes

--- src/components/entityNote/entityNote.ts
+++ src/components/entityNote/entityNote.ts
@@ -4,7 +4,7 @@ import { getEndpointName } from "../../utils/api.utils";
 
 export const notes: { [key: string]: string } = {};
 
-export const getEntityNoteComponent = (entity: Entity) => {
+export const getEntityNoteComponent = (entity: Entity, saveArea?: HTMLElement) => {
 
 	// Note: This replaces the newlines with HTML breaks so the notes are true to their input.
 	// note.replace(/\n/g, "<br>")
@@ -27,34 +27,52 @@ export const getEntityNoteComponent = (entity: Entity) => {
 
 	// When clicking the notes, enter edit mode.
 	p.addEventListener("click", () => {
+		// Do not create multiple textarea
+		if (section.contains(document.querySelector("textarea"))) 
+			return;
+
 		const textarea = document.createElement("textarea");
 		textarea.value = note || "";
-		const saveButton = document.createElement("button");
-		saveButton.textContent = "Save";
 
 		const saveNote = () => {
 			note = textarea.value;
 			notes[key] = note;
 			p.innerHTML = note ? note.replace(/\n/g, "<br>") : "No notes.";
 			p.className = note ? "unselectable" : "entity-note-empty unselectable";
-			section.replaceChild(p, textarea);
-			section.removeChild(saveButton);
 
-			// Scroll P to bottom so it's "in line" with the input area
+			// Only replace if textarea is still in the DOM
+			if (section.contains(textarea)) {
+				section.replaceChild(p, textarea);
+			}
+
+			// Scroll P to bottom so it's "in-line" with the input area
 			p.scrollTop = p.scrollHeight;
+
+			console.log(notes);
 		};
 
-		saveButton.addEventListener("click", saveNote);
+		const handleMouseDown = (event: MouseEvent) => {
+			if (event.target === saveArea) {
+				saveNote();
+				saveArea?.removeEventListener("mousedown", handleMouseDown);
+			}
+		};
+
+		saveArea?.addEventListener("mousedown", handleMouseDown);
 
 		textarea.addEventListener("keypress", (event) => {
 			if (event.key === "Enter" && !event.shiftKey) {
 				event.preventDefault();
 				saveNote();
+				saveArea?.removeEventListener("mousedown", handleMouseDown);
 			}
 		});
 
-		section.replaceChild(textarea, p);
-		section.appendChild(saveButton);
+		// Ensure we replace only if p is still there
+		if (section.contains(p)) {
+			section.replaceChild(textarea, p);
+		}
+
 		textarea.focus();
 	});
 
